f0314dae129393d9bad66d6c874af185a33ba9b4
AnasTheAnalyst <84326568+anasAllahwala@users.noreply.github.com>
2022-11-30 23:49:38 +0500

    Create Main.java

---
+++ Phase 2/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        CU CPU = new CU();
+        String dirname = "C:\\Users\\ibteh\\IdeaProjects\\OS_Project\\src\\demo_files";
+
+
+        RunFilesFromDirectory(dirname, CPU);
+
+        // Run program i.e. cpu will start processing instruction
+         CPU.execute();
+
+//        readFile("C:\\Users\\ibteh\\IdeaProjects\\OS_Project\\src\\demo_files\\noop");
+    }
+
+    public static void RunFilesFromDirectory(String dirname, CU CPU) throws Exception{
+
+            File dir = new File(dirname);
+            for (File file : dir.listFiles()) {
+//                System.out.println(file.getPath());
+                CPU.loadProgram(file.getPath());
+//                readFile(file.getPath());
+
+            }
+
+    }
+
+    public static void readFile(String filename) throws Exception {
+        DataInputStream BUFFER = new DataInputStream(new BufferedInputStream(new FileInputStream( filename ) ) );
+        int value = 0;
+        int counter = 0;
+        while(value != -1){
+            value = BUFFER.read();
+            System.out.println(counter++ + ")\t\t" + value);
+        }
+    }
+}
